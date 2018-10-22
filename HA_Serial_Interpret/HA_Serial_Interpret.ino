c41a062fb5c5cb1bd3567f436e2abc01472278b0
ironzap2 <CharlesAlbo@gmail.com>
2018-10-21 20:22:25 -0400

    Add relay control, untested

--- HA_Serial_Interpret/HA_Serial_Interpret.ino
+++ HA_Serial_Interpret/HA_Serial_Interpret.ino
@@ -2,13 +2,17 @@ const byte numChars = 32;
 char receivedChars[numChars];
 boolean NewData = false;
 
-#define LightRelay = A0;
-#define FanRelay = A1;
+#define LightSensor A0
+#define LightRelay 2
+
+#define FanSensor A1
+#define FanRelay 3
 
 char* circuit;
 char* command;
+float current;
 
-void setup(void)
+void setup()
   {
     // Setup Serial and wait until it is ready.
     Serial.begin(9600);
@@ -16,18 +20,20 @@ void setup(void)
     Serial.println("Arduino is ready.");
   }
 
-void loop(void)
+void loop()
   {
-    ReadData(circuit,command);
+    ReadData();
     
     if (NewData == true)
       {
-        UseData(circuit, command);
+        UseData();
       }
+    
+    delay(500);
   }
   
 //_________________________________________________________________________  
-void ReadData(char* CircuitOut,char* CommandOut)
+void ReadData()
   {
     static boolean recvInProgress = false;
     static byte ndx = 0;
@@ -63,34 +69,78 @@ void ReadData(char* CircuitOut,char* CommandOut)
             recvInProgress = true;
           }
       }
-    
-    CircuitOut = strtok(receivedChars, ",");
-    CommandOut = strtok(NULL, ",");
-    return;
   }
   
 //_________________________________________________________________________ 
-char UseData(char* CircuitIn,char* CommandIn) 
-  {
-    char CircuitChar = char(CircuitIn);
-    char CommandChar = char(CommandIn);
-    
+void UseData() 
+  { 
     NewData = false;
+    circuit = strtok(receivedChars, ",");
+    command = strtok(NULL, ",");
     
     Serial.println("USE DATA");
     Serial.print("Circuit: ");
-    Serial.println(CircuitChar);
+    Serial.println(circuit);
     Serial.print("Command: ");
-    Serial.println(CommandChar);
+    Serial.println(command);
     
-    switch (CircuitChar)
+    // Tell ReadCurrent() which sensor to read
+    if (strcmp(circuit, "Lights") == 0)
       {
-        case 'Lights':
-          Serial.println("CHANGING LIGHTS");
-          break;
-        case 'Fan':
-          Serial.println("CHANGING FAN");
-          break;
+        ToggleRelay(LightSensor, LightRelay);
       }
+    else if (strcmp(circuit, "Fan") == 0)
+      {
+        ToggleRelay(FanSensor, FanRelay);
+      }
+  }
+
+//_________________________________________________________________________ 
+void ToggleRelay(const int sensor, const int relay)
+  {
+    current = ReadCurrent(sensor);
+    
+    if (strcmp(command, "ON") == 0 && current == 0)
+      {
+        digitalWrite(relay, !digitalRead(relay));
+      }
+    else if (strcmp(command, "OFF") == 0 && current != 0)
+      {
+        digitalWrite(relay, !digitalRead(relay));
+      }
+  }
+
+//_________________________________________________________________________ 
+float ReadCurrent(const int sensor)
+  {
+    float result;
+    int ReadValue;
+    int MaxValue = 0;
+    unsigned long StartTime = millis();
+    
+    while ((millis()-StartTime) < 1000)
+      {
+        ReadValue = analogRead(sensor);
+        if (ReadValue > MaxValue) 
+          {
+            MaxValue = ReadValue;
+          }
+      }
+    
+   // Convert the digital data to a voltage
+   // result = (maxValue * 5.0)/1024.0;
+   
+   // Use Ohms law to calculate current across resistor and express in mA 
+   // nCurrThruResistorPP = (nVPP/200.0) * 1000.0;
+   
+   //Use Formula for SINE wave to convert to RMS 
+   // nCurrThruResistorRMS = nCurrThruResistorPP * 0.707;
+   
+   // Current Transformer Ratio is 1000:1...
+   // nCurrentThruWire = nCurrThruResistorRMS * 1000;
+   
+   result = MaxValue * 17.2607;
+   
+   return result;
   }
 
